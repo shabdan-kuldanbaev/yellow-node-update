ff2ef6b399998fb713564d660ee9828018c11f90
Артем Павленко <itemka@Artems-MacBook-Pro.local>
2020-12-11 01:31:50 +0300

    Fix widget.clutch.co

--- components/CompanyCommon/Awards/index.jsx
+++ components/CompanyCommon/Awards/index.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Animated } from 'components';
 import { animatedType } from 'utils/constants';
 import { awards } from './utils/data';
 import styles from './styles.module.scss';
 
-export const Awards = ({ awards }) => (
-  <div className={styles.awards}>
-    <div
-      className="clutch-widget"
-      data-url="https://widget.clutch.co"
-      data-widget-type="1"
-      data-height="50"
-      data-clutchcompany-id="381822"
-    />
-    {/* {awards && awards.map((award, index) => (
+export const Awards = ({ awards }) => {
+  const [hostname, setHost] = useState('');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHost(window.location.hostname);
+    }
+  }, [hostname]);
+
+  return (
+    <div className={styles.awards}>
+      <iframe
+        id="iframe-0.824960673393011"
+        src={`https://widget.clutch.co/widgets/get/1?ref_domain=${hostname}&uid=381822&ref_path=/company`}
+        title="clutch-widget"
+      />
+      {/* {awards && awards.map((award, index) => (
       <Animated
         key={`award/${index}`}
         type={animatedType.isCustom}
@@ -26,8 +33,9 @@ export const Awards = ({ awards }) => (
         <img src={award.image} alt={`award/${index}`} />
       </Animated>
     ))} */}
-  </div>
-);
+    </div>
+  );
+};
 
 Awards.defaultProps = {
   awards,
