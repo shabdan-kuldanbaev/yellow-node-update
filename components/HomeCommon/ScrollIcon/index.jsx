f355a1a49a9861eca71d840ae4dd821314745e16
Ksenia Nesterovich <kseniya.nestsiarovich@yellow.systems>
2021-02-25 15:11:43 +0300

    fix: - change way to adding svg

--- components/HomeCommon/ScrollIcon/index.jsx
+++ components/HomeCommon/ScrollIcon/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ScrollDown from './images/scroll-down.svg';
 import styles from './styles.module.scss';
 
 export const ScrollIcon = () => (
   <div className={styles.iconWrapper}>
-    <div className={styles.scrollDown} />
+    <div className={styles.scrollDown}>
+      <img src={ScrollDown} alt="scroll-down" />
+    </div>
     <div className={styles.arrow}>
       <span />
       <span />
