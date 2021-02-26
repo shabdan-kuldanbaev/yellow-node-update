b1752d69df2bcf3571931a41b551ac97c2e36c93
Артем Павленко <itemka@Artems-MacBook-Pro.local>
2021-02-26 03:21:29 +0300

    Fix the next-redux-wrapper import

--- redux/store.js
+++ redux/store.js
@@ -1,14 +1,20 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { createWrapper } from 'next-redux-wrapper';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import rootReducers from 'redux/reducers';
 import rootSaga from 'redux/sagas';
 
-const bindMiddleware = (middleware) => (process.env.NODE_ENV !== 'production'
-  ? composeWithDevTools(applyMiddleware(...middleware, logger))
-  : applyMiddleware(...middleware));
+const bindMiddleware = (middleware) => {
+  if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line
+    const { composeWithDevTools } = require('redux-devtools-extension');
+
+    return composeWithDevTools(applyMiddleware(...middleware, logger));
+  }
+
+  return applyMiddleware(...middleware);
+};
 
 function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
