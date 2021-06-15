7ee69f1d8ed45414bb43a844bacdb6ab2eb28ec2
Tim-Osipchik <tsimafei.osipchyk@yellow.systems>
2021-06-15 14:59:21 +0300

    added animation to wireframes

--- components/CaseStudiesCommon/Wireframe/index.jsx
+++ components/CaseStudiesCommon/Wireframe/index.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
+import Swiper from 'react-id-swiper';
+import SwiperCors, { Autoplay } from 'swiper';
 import { Animated } from 'components/Common/Animated';
 import { ANIMATED_TYPE } from 'utils/constants';
 import styles from './styles.module.scss';
 
-const Wireframe = ({ imageUrl, type }) => (
-  <Animated
-    type={ANIMATED_TYPE.isFade}
-    delay={500}
-    duration={1000}
-  >
-    <div className={cn(styles.imagesContainer, styles[type])}>
-      <img
-        src={imageUrl}
-        className={styles.image}
-        alt=""
-      />
-    </div>
-  </Animated>
-);
+SwiperCors.use([Autoplay]);
+
+const Wireframe = ({ imageUrl, type }) => {
+  const params = {
+    loop: true,
+    autoplay: {
+      delay: 1,
+    },
+    freeMode: true,
+    spaceBetween: 80,
+    speed: 10000,
+  };
+
+  return (
+    <Animated
+      type={ANIMATED_TYPE.isFade}
+      delay={500}
+      duration={1000}
+    >
+      <div className={cn(styles.container, styles[type])}>
+        <Swiper {...params}>
+          <img
+            src={imageUrl}
+            className={styles.image}
+            alt=""
+          />
+        </Swiper>
+      </div>
+    </Animated>
+  );
+};
 
 Wireframe.prototype = {
   imageUrl: PropTypes.string.isRequired,
@@ -27,3 +45,19 @@ Wireframe.prototype = {
 };
 
 export default Wireframe;
+
+// <Animated
+//   type={ANIMATED_TYPE.isFade}
+//   delay={500}
+//   duration={1000}
+// >
+//   <div className={cn(styles.imagesContainer, styles[type])}>
+//     <Swiper {...params}>
+//       <div>Slide #1</div>
+//       <div>Slide #2</div>
+//       <div>Slide #3</div>
+//       <div>Slide #4</div>
+//       <div>Slide #5</div>
+//     </Swiper>
+//   </div>
+// </Animated>
