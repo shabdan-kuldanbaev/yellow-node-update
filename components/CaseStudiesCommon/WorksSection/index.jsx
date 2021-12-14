7031ab9f77d0b0b89b053806114405692d00a57e
Nikita Kuchko <nikita.kuchko@yellow.systems>
2021-12-14 16:26:37 +0300

    Fix some problems

--- components/CaseStudiesCommon/WorksSection/index.jsx
+++ components/CaseStudiesCommon/WorksSection/index.jsx
@@ -4,12 +4,14 @@ import SectionTitle from 'components/CaseStudiesCommon/SectionTitle';
 import { getFileUrl, getOptimizedContentfulImage } from 'utils/helper';
 import styles from './styles.module.scss';
 
-const WireframesSection = ({ data, type }) => {
+const WorksSection = ({ data, type }) => {
   const sectionBackgroundImage = getOptimizedContentfulImage(
     getFileUrl(get(data, 'background', {})),
     { fm: 'png' },
   );
-  const sectionStyle = sectionBackgroundImage ? { backgroundImage: `url(${sectionBackgroundImage})` } : {};
+  const sectionStyle = sectionBackgroundImage
+    ? { backgroundImage: `url(${sectionBackgroundImage})` }
+    : {};
 
   return (
     <section
@@ -21,24 +23,32 @@ const WireframesSection = ({ data, type }) => {
         type={type}
       />
       <div className={styles.sequenceOfSteps}>
-        {data.contentModules.map(({ fields }, index) => (
-          fields.title ? (
-            <div className={styles.step}>
-              <div className={styles.markNumber}>
-                <div className={styles.contentMarkNumber}>{index + 1}</div>
+        {data.contentModules
+          && data.contentModules.map(({ fields }, index) => {
+            if (!fields.title) {
+              return null;
+            }
+
+            return (
+              <div className={styles.step}>
+                <div className={styles.markNumber}>
+                  <h3 className={styles.contentMarkNumber}>
+                    {index + 1}
+                  </h3>
+                </div>
+                <h3 className={styles.stepTitle}>
+                  {fields.title}
+                </h3>
               </div>
-              <div className={styles.stepTitle}>{fields.title}</div>
-            </div>
-          )
-            : null
-        ))}
+            );
+          })}
       </div>
       <div className={styles.imagesConatiner}>
-        {data.images.map(({ fields }) => (
+        {data.images && data.images.map(({ fields }) => (
           <img
             className={styles.image}
             src={fields.file.url}
-            alt="123"
+            alt={fields.title}
           />
         ))}
       </div>
@@ -46,4 +56,4 @@ const WireframesSection = ({ data, type }) => {
   );
 };
 
-export default WireframesSection;
+export default WorksSection;
