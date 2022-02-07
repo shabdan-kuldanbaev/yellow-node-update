bcb59d5f562d29c13cf3d1a5f6185e03dbc86f4d
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-02-07 14:54:14 +0300

    Code structure improvement

--- containers/CustomService/index.jsx
+++ containers/CustomService/index.jsx
@@ -7,60 +7,12 @@ import { MetaTags } from 'components/Common/MetaTags';
 import { FullScreenEstimation } from 'components/Common/FullScreenEstimation';
 import { AppDevelopmentCommon } from 'components/AppDevelopmentCommon';
 import { getDocumentFields, rootUrl } from 'utils/helper';
-import { PAGES, CONTACT_FORM_TITLES } from 'utils/constants';
+import { CONTACT_FORM_TITLES, PAGES } from 'utils/constants';
 import { useRouter } from 'next/router';
 import cn from 'classnames';
 import { FeedbackFormContainer } from 'containers/Home/FeedbackForm';
-import { microdata } from 'utils/microdata';
-import { pagesBreadcrumbs } from 'utils/breadcrumbs';
 import styles from './styles.module.scss';
-
-// TODO: extract this thing to somewhere
-const getServicePageInfo = (pathname) => {
-  switch (pathname) {
-  case PAGES.androidDevelopmentServices:
-    return {
-      pageMicrodata: microdata.customAndroidApp(),
-      breadcrumbs: pagesBreadcrumbs.androidDevelopmentServices(),
-    };
-
-  case PAGES.developmentServices:
-    return {
-      pageMicrodata: microdata.customIOSApp(),
-      breadcrumbs: pagesBreadcrumbs.developmentServices(),
-    };
-
-  case PAGES.customChatApp:
-    return {
-      pageMicrodata: microdata.customChatApp(),
-      breadcrumbs: pagesBreadcrumbs.customChatApp(),
-    };
-
-  case PAGES.customMobileApp:
-    return {
-      pageMicrodata: microdata.customMobileApp(),
-      breadcrumbs: pagesBreadcrumbs.customMobileApp(),
-    };
-
-  case PAGES.customWebApp:
-    return {
-      pageMicrodata: microdata.customWebApp(),
-      breadcrumbs: pagesBreadcrumbs.customWebApp(),
-    };
-
-  case PAGES.designServices:
-    return {
-      pageMicrodata: microdata.designServices(),
-      breadcrumbs: pagesBreadcrumbs.designServices(),
-    };
-
-  default:
-    return {
-      pageMicrodata: null,
-      breadcrumbs: null,
-    };
-  }
-};
+import { getServicePageInfo } from './utils/servicePageHelper';
 
 const CustomServiceContainer = ({
   pageData,
