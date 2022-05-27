f0209665da09a1e6b76a78605b5c8e398e5bb39a
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-05-27 13:29:56 +0300

    Added pagination

--- components/PortfolioCommon/index.jsx
+++ components/PortfolioCommon/index.jsx
@@ -1,19 +1,24 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withScroll } from 'hocs/withScroll';
-import { DEFAULT_WORK_TYPE, ROUTES } from 'utils/constants';
+import { ANIMATED_TYPE, DEFAULT_WORK_TYPE, ROUTES } from 'utils/constants';
 import gaHelper from 'utils/ga';
 import TypeSelector from './TypeSelector';
 import TagsSelector from './TagsSelector';
 import Work from './Work';
-import { filterWorks } from './utils';
+import { DEFAULT_WORKS_LIMIT, filterWorks } from './utils';
 import styles from './styles.module.scss';
+import { getLimitedList } from '../../utils/helper';
+import ButtonMore from '../Common/ButtonMore';
+import { Animated } from '../Common/Animated';
 
 const Portfolio = ({
   works,
   maxScrollPosition,
 }) => {
+  const [filteredWorks, setFilteredWorks] = useState([]);
   const [worksDisplay, setWorksDisplay] = useState([]);
+  const [currentLimit, setCurrentLimit] = useState(DEFAULT_WORKS_LIMIT);
   const [selectedType, setSelectedType] = useState(DEFAULT_WORK_TYPE);
   const [selectedTags, setSelectedTags] = useState([]);
 
@@ -25,13 +30,19 @@ const Portfolio = ({
     BibleMania: 'biblemania',
   };
 
+  const isFilteringEnabled = filteredWorks.length !== works.length;
+  const hasHiddenItems = !isFilteringEnabled && worksDisplay.length !== filteredWorks.length;
+
   const onSelectedTypeChange = useCallback((type) => {
     if (type === selectedType) return;
 
+    setCurrentLimit(DEFAULT_WORKS_LIMIT);
     setSelectedType(type);
   }, [selectedType]);
 
   const onSelectedTagsChange = useCallback((tag) => {
+    setCurrentLimit(DEFAULT_WORKS_LIMIT);
+
     if (selectedTags.includes(tag)) {
       return setSelectedTags((prev) => prev.filter((selectedTag) => selectedTag !== tag));
     }
@@ -39,11 +50,23 @@ const Portfolio = ({
     setSelectedTags((prev) => [...prev, tag]);
   }, [selectedTags]);
 
+  const onShowMoreClick = useCallback(() => {
+    setCurrentLimit((prev) => prev + DEFAULT_WORKS_LIMIT);
+  }, []);
+
   useEffect(() => {
     if (!works.length) return;
 
-    setWorksDisplay(filterWorks(works, { tags: selectedTags, workType: selectedType }));
+    setFilteredWorks(filterWorks(works, { tags: selectedTags, workType: selectedType }));
   }, [selectedType, selectedTags, works]);
+
+  useEffect(() => {
+    if (isFilteringEnabled) {
+      return setWorksDisplay(filteredWorks);
+    }
+
+    setWorksDisplay(getLimitedList(filteredWorks, { limit: currentLimit }));
+  }, [isFilteringEnabled, currentLimit, filteredWorks]);
 
   useEffect(() => () => {
     gaHelper.trackEvent(
@@ -74,6 +97,21 @@ const Portfolio = ({
           />
         ))}
       </div>
+      {hasHiddenItems && (
+        <Animated
+          type={ANIMATED_TYPE.isCustom}
+          translateY="2.82352941em"
+          opasityDuration={1}
+          transformDuration={1}
+          transitionDelay={250}
+        >
+          <ButtonMore
+            title="See more projects"
+            buttonStyle={styles.showMoreButton}
+            handleOnClick={onShowMoreClick}
+          />
+        </Animated>
+      )}
     </>
   );
 };
