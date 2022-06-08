f3831cf094e712087299d4a0da5caa180f9a9299
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-06-08 18:28:02 +0800

    Removed animated for

--- components/PortfolioCommon/SelectorElement/index.js
+++ components/PortfolioCommon/SelectorElement/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-import { Animated } from 'components/Common/Animated';
 import styles from './styles.module.scss';
-import { animatedProps } from './utils';
 
 const SelectorElement = ({
   displayName,
@@ -11,18 +9,14 @@ const SelectorElement = ({
   selected,
   className,
 }) => (
-  <Animated
-    {...animatedProps}
-    transitionDelay={250}
+  <button
+    type="button"
+    onClick={onClick}
+    className={cn(styles.selectorElement, styles[type], { [styles.selected]: selected }, className)}
   >
-    <button
-      type="button"
-      onClick={onClick}
-      className={cn(styles.selectorElement, styles[type], { [styles.selected]: selected }, className)}
-    >
-      {displayName}
-    </button>
-  </Animated>
+    {displayName}
+  </button>
+
 );
 
 export default SelectorElement;
