1e975ba4f97678cd275a344729561cdb6cf29b89
Tim Osipchik <tsimafei.osipchyk@yellow.systems>
2022-06-09 12:34:28 +0300

    added styles for new case

--- components/CaseStudiesCommon/Intro/index.jsx
+++ components/CaseStudiesCommon/Intro/index.jsx
@@ -28,6 +28,7 @@ export const Intro = (props) => {
   case CASE_STUDIES.telemojo:
   case CASE_STUDIES.smartcenter:
   case CASE_STUDIES.bionorica:
+  case CASE_STUDIES.goodPsychics:
     return <VerticalIntro {...props} />;
   default:
     return null;
