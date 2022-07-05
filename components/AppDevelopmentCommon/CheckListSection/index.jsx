60c04eb783babdcbad7641695c7988d1774f0623
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-07-06 01:03:31 +0800

    Fixed unique keys error in CheckList section

--- components/AppDevelopmentCommon/CheckListSection/index.jsx
+++ components/AppDevelopmentCommon/CheckListSection/index.jsx
@@ -46,7 +46,7 @@ export const CheckListSection = ({
 
             return (
               <Animated
-                key={`check-list/${itemTitle}`}
+                key={`check-list/${index}`}
                 {...animationProps}
                 transitionDelay={250 + 50 * index}
               >
