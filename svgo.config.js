979aa48c014b96b046a13bb129d01d8c70bd0cec
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-07-11 15:44:55 +0800

    Added svgo

---
+++ svgo.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          // customize default plugin options
+          inlineStyles: {
+            onlyMatchedOnce: false,
+          },
+        },
+      },
+    },
+  ],
+};
