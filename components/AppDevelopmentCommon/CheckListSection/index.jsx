8d4f1068a20943090bec3f336cbe4b1c6c3c7390
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-07-22 05:36:17 +0800

    Refactored UxUiDev Cards section

--- components/AppDevelopmentCommon/CheckListSection/index.jsx
+++ components/AppDevelopmentCommon/CheckListSection/index.jsx
@@ -36,6 +36,7 @@ export const CheckListSection = ({
         <SectionTitle
           title={title}
           description={description}
+          titleStyle={styles.titleStyle}
         />
         <div className={styles.checkList}>
           {listData.map((item, index) => {
