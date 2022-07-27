c6554141637b881f988bcd2d20dd5717b7c7330a
Nikita Kuchko <nikita.kuchko@yellow.systems>
2022-07-27 17:34:54 +0300

    Fixed incorrect display of tags on the Works page

--- components/PortfolioCommon/TypeSelector/.config.js
+++ components/PortfolioCommon/TypeSelector/.config.js
@@ -2,7 +2,6 @@ import styles from './styles.module.scss';
 
 export const SWIPER_CONFIG = {
   containerClass: styles.container,
-  slideClass: styles.slide,
   slidesPerView: 'auto',
   passiveListeners: true,
   speed: 500,
