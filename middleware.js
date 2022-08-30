9c05919553b23da9a9dc7f9c32c0fcb6766a1fba
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-08-31 00:00:49 +0300

    Extracted redirects

--- middleware.js
+++ middleware.js
@@ -1,63 +1,106 @@
 import { NextResponse } from 'next/server';
-import { CUSTOM_DOMAIN, INDEX_FILES } from 'utils/constants';
-import { getNewPathname, isPage, isUrlChanged } from 'utils/middlewares';
+import {
+  CUSTOM_DOMAIN,
+  INDEX_FILES,
+  IS_PROD,
+} from 'utils/constants';
+import { getNewPathname, isPage } from 'utils/middlewares';
 
-export function middleware(req) {
-  const {
-    host, // localhost:3000 | yellow.systems | yws-dev.xyz
-    hostname, // localhost | yellow.systems | yws-dev.xyz
-    pathname, // /url/path
-  } = req.nextUrl;
+const forceHttps = (req) => {
+  const host = req.headers.get('host') || '';
 
-  const {
-    headers,
-    method,
-  } = req;
+  if (
+    IS_PROD
+    && !host.includes('localhost')
+    && req.headers.get('x-forwarded-proto') !== 'https'
+  ) {
+    return NextResponse.redirect(`https://${host}${req.nextUrl.pathname}`, 301);
+  }
+};
+
+const redirectWwwToNonWww = (req) => {
+  const host = req.headers.get('host') || '';
+  const proto = req.headers.get('x-forwarded-proto');
+  const wwwRegex = /^www\./;
+
+  if (!host.includes('localhost') && wwwRegex.test(host)) {
+    const newHost = host.replace(wwwRegex, '');
+
+    return NextResponse.redirect(`${proto}://${newHost}${req.nextUrl.pathname}`, 301);
+  }
+};
+
+const redirectToCustomDomain = (req) => {
+  const host = req.headers.get('host') || '';
+
+  if (req.headers.get('user-agent') === 'Amazon CloudFront') {
+    return;
+  }
+
+  if (host.includes('yellow-systems-nextjs-prod') || host.includes('yellow.id')) {
+    return NextResponse.redirect(`https://${CUSTOM_DOMAIN}${req.nextUrl.pathname}`, 301);
+  }
+};
+
+const redirectToLowerCasePath = (req) => {
+  const host = req.headers.get('host') || '';
+  const proto = req.headers.get('x-forwarded-proto');
+  const { pathname } = req.nextUrl;
+
+  const pathnameLowerCase = pathname.toLowerCase();
+
+  if (isPage(pathnameLowerCase) && pathnameLowerCase !== pathname) {
+    return NextResponse.redirect(`${proto}://${host}${pathnameLowerCase}`, 301);
+  }
+};
+
+const redirectToNewPath = (req) => {
+  const host = req.headers.get('host') || '';
+  const proto = req.headers.get('x-forwarded-proto');
+  const { pathname } = req.nextUrl;
+  const newPathname = getNewPathname(pathname);
+
+  if (newPathname) {
+    return NextResponse.redirect(`${proto}://${host}${newPathname}`, 301);
+  }
+};
+
+const redirectFromIndexFile = (req) => {
+  const host = req.headers.get('host') || '';
+  const proto = req.headers.get('x-forwarded-proto');
+  const { pathname } = req.nextUrl;
+
+  if (INDEX_FILES.includes(pathname)) {
+    return NextResponse.redirect(`${proto}://${host}`, 301);
+  }
+};
+
+const processMiddlewareFunctions = (req, middlewareFns) => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const middlewareFn of middlewareFns) {
+    const fnResponse = middlewareFn(req);
+
+    if (fnResponse) {
+      return fnResponse;
+    }
+  }
+
+  return NextResponse.next();
+};
+
+export const middleware = (req) => {
+  const { pathname } = req.nextUrl;
 
   if (pathname.includes('_next')) {
     return NextResponse.next();
   }
 
-  const url = req.nextUrl.clone();
-
-  /* TODO: Test if there's no need in xhr check */
-  if (method === 'GET' && host.includes('www.')) {
-    url.host = host.substring(4);
-  }
-
-  if (hostname.includes('yellow-systems-nextjs-prod') && headers.get('user-agent') !== 'Amazon CloudFront') {
-    url.host = CUSTOM_DOMAIN;
-  }
-
-  if (hostname === 'yellow.id') {
-    url.host = CUSTOM_DOMAIN;
-  }
-
-  if (INDEX_FILES.includes(pathname)) {
-    url.pathname = '/';
-  }
-
-  const pathnameLowerCase = pathname.toLowerCase();
-
-  if (isPage(pathname) && pathnameLowerCase !== pathname) {
-    url.pathname = pathnameLowerCase;
-  }
-
-  const newPathname = getNewPathname(pathname);
-
-  if (newPathname) {
-    url.pathname = newPathname;
-  }
-
-  if (isUrlChanged(req.nextUrl, url)) {
-    // eslint-disable-next-line no-console
-    console.log({ // This log is for tracing redirects. Server-side only
-      from: req.nextUrl.toString(),
-      to: url.toString(),
-    });
-
-    return NextResponse.redirect(url, 301);
-  }
-
-  return NextResponse.next();
-}
+  return processMiddlewareFunctions(req, [
+    forceHttps,
+    redirectWwwToNonWww,
+    redirectToCustomDomain,
+    redirectToLowerCasePath,
+    redirectToNewPath,
+    redirectFromIndexFile,
+  ]);
+};
