1805f3bd853ebb5871ac08dfdd1f46b0073a14b1
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-09-08 23:57:52 +0300

    Disabled sentry debug output

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,12 +1,11 @@
 import * as Sentry from '@sentry/nextjs';
 
-const dev = process.env.NODE_ENV !== 'production';
 const environment = process.env.SENTRY_ENVIRONMENT || process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
 const dsn = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn,
   environment,
-  debug: dev,
+  debug: false,
   tracesSampleRate: 1.0,
 });
