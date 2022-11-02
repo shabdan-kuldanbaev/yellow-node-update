bd006b9f263bf81c6e318dcd422b705c35e62da8
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-11-03 03:45:09 +0600

    Removed junk code and file

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,18 +8,10 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    // "storybook-addon-next",
     '@storybook/preset-scss',
   ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
-  },
-  refs: {
-    'design-system': {
-      title: "Storybook Design System",
-      url: "https://5ccbc373887ca40020446347-yldsqjoxzb.chromatic.com",
-      expanded: false // optional, true by default
-    }
-   }
+  }
 }
