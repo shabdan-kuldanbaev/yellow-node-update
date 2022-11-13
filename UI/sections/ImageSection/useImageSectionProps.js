e0e28b017374a3c5ed7ba43c526d21dec2b3507f
Shabdan <shabdan@mancho.dev>
2022-11-13 18:01:24 +0600

    Refactored image section Created new hook for section

---
+++ UI/sections/ImageSection/useImageSectionProps.js
@@ -0,0 +1,30 @@
+import get from 'lodash/get';
+import { useMemo } from 'react';
+import { getDocumentFields, getFileUrl } from '../../../utils/helper';
+
+export default (props) => {
+  const {
+    title,
+    description,
+    images,
+    contentModules,
+  } = useMemo(() => getDocumentFields(
+    props,
+    [
+      'title',
+      'description',
+      'images',
+      'contentModules',
+    ],
+  ), [props]);
+
+  const { text } = getDocumentFields(get(contentModules, '[0]', {}));
+  const imageUrl = getFileUrl(get(images, '[0]'));
+
+  return {
+    title,
+    description,
+    text,
+    imageUrl,
+  };
+};
