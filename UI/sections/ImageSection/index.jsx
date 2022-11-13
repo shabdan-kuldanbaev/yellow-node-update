d08cf728d1f0b070160c5e1b59aa78e571fbf975
Shabdan <shabdan@mancho.dev>
2022-11-13 18:01:04 +0600

    Refactored image section

--- UI/sections/ImageSection/index.jsx
+++ UI/sections/ImageSection/index.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import ContentfulParser from 'components/BlogCommon/Article/ContentfulParser';
 import Animated from 'components/Common/Animated';
 import { SectionTitle } from 'components/AppDevelopmentCommon/SectionTitle';
-import { getImageSectionProps } from './utils/imageHelper';
+import { REVEAL_ANIMATION_PROPS } from '../../../utils/constants';
+import useImageSectionProps from './useImageSectionProps';
 import styles from './styles.module.scss';
 
 const ImageSection = ({
@@ -15,18 +16,15 @@ const ImageSection = ({
     description,
     text,
     imageUrl,
-    animatedProps,
-  } = getImageSectionProps(sectionData);
+  } = useImageSectionProps(sectionData);
 
-  if (!imageUrl || !text) {
-    return null;
-  }
+  if (!imageUrl || !text) return null;
 
   return (
     <section className={styles[type]}>
       <div className={styles.imageSection}>
         <Animated
-          {...animatedProps}
+          {...REVEAL_ANIMATION_PROPS}
           transitionDelay={250}
         >
           <div
@@ -42,7 +40,7 @@ const ImageSection = ({
             titleStyle={styles.titleStyle}
           />
           <Animated
-            {...animatedProps}
+            {...REVEAL_ANIMATION_PROPS}
             transitionDelay={350}
           >
             <ContentfulParser document={text} />
