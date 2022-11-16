f6998b798d5597934c4f4a438cfad96f3d7db91a
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-11-17 01:47:49 +0600

    Replace Figma prototype with component

--- components/CaseStudiesCommon/CaseStudyPrototype/index.jsx
+++ components/CaseStudiesCommon/CaseStudyPrototype/index.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import get from 'lodash/get';
-import { useSelector } from 'react-redux';
-import { selectIsMobileResolutions } from 'redux/selectors/layout';
 import SectionTitle from 'components/CaseStudiesCommon/SectionTitle';
+import FigmaPrototype from 'components/Common/FigmaPrototype';
 import { getDocumentFields } from 'utils/helper';
 import styles from './styles.module.scss';
 
 const CaseStudyPrototype = ({ data, type }) => {
-  const isMobileResolutions = useSelector(selectIsMobileResolutions);
-
   const contentModules = get(data, 'contentModules');
 
   if (!contentModules) {
@@ -26,16 +23,12 @@ const CaseStudyPrototype = ({ data, type }) => {
         type={type}
       />
       <div className={styles.prototypeWrapper}>
-        <div className={styles.prototypeContainer}>
-          <iframe
-            height={isMobileResolutions ? '650' : '800'}
-            width={isMobileResolutions ? '350' : '500'}
-            src={`https://www.figma.com/embed?embed_host=astra&url=${url}`}
-            title={data.title}
-            allowTransparency
-            seamless
+        {url && (
+          <FigmaPrototype
+            src={url}
+            className={styles.prototype}
           />
-        </div>
+        )}
       </div>
     </section>
   );
