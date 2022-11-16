407711646d4338aa244683a86fd5fbcd5034ef35
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-11-17 01:29:42 +0600

    Added Figma prototype component

---
+++ components/Common/FigmaPrototype/index.jsx
@@ -0,0 +1,48 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import { CUSTOM_DOMAIN } from 'utils/constants';
+import styles from './styles.module.scss';
+
+const FigmaPrototype = ({
+  src,
+  deviceFrameSrc = '/images/common/devices/iPhone.png',
+  className,
+}) => {
+  const url = useMemo(() => {
+    const prototypeUrl = new URL('https://www.figma.com/embed');
+
+    prototypeUrl.searchParams.set('embed_host', CUSTOM_DOMAIN);
+    prototypeUrl.searchParams.set('url', src);
+
+    return prototypeUrl;
+  }, [src]);
+
+  return (
+    <div className={cn(styles.prototypeContainer, className)}>
+      <iframe
+        src={url}
+        className={styles.prototype}
+        frameBorder="0"
+        title="Figma prototype "
+        seamless
+      />
+      <img
+        src={deviceFrameSrc}
+        className={styles.deviceFrame}
+        alt=""
+      />
+    </div>
+  );
+};
+
+FigmaPrototype.defaultProps = {
+  className: null,
+};
+
+FigmaPrototype.propTypes = {
+  src: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+export default FigmaPrototype;
