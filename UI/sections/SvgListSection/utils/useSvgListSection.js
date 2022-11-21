59fa2af4ddec17006d206fe3bc63f2aa7c120857
Nikita Kuchko <nikita.kuchko@yellow.systems>
2022-11-21 12:26:21 +0300

    Refactor code

--- UI/sections/SvgListSection/utils/useSvgListSection.js
+++ UI/sections/SvgListSection/utils/useSvgListSection.js
@@ -1,6 +1,5 @@
 import {
   useEffect,
-  useCallback,
   useMemo,
   useState,
 } from 'react';
@@ -25,8 +24,6 @@ export const useSvgListSection = ({
 
   const displayNames = iconsGroups.map((group) => getDocumentFields(group, ['title']).title);
 
-  const handleSelectedGroupIndexChange = useCallback((i) => setSelectedGroupIndex(i), []);
-
   useEffect(() => {
     if (!withSelector) {
       return;
@@ -44,8 +41,8 @@ export const useSvgListSection = ({
     handleOnCTAClick,
     type,
     withSelector,
-    handleSelectedGroupIndexChange,
     displayNames,
     selectedGroupIndex,
+    handleSelectedGroupIndexChange: setSelectedGroupIndex,
   };
 };
