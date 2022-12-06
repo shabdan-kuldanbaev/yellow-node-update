1d87163887b7ab1207b8596ea2fd5210c715943f
Nikita Kuchko <nikita.kuchko@yellow.systems>
2022-12-06 13:54:04 +0300

    Add new section in mobile-app-development-company

--- UI/sections/DownloadSection/index.jsx
+++ UI/sections/DownloadSection/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import CallToAction from 'components/Common/CallToAction';
-import { SectionTitle } from 'components/AppDevelopmentCommon/SectionTitle';
+import SectionTitle from 'UI/components/SectionTitle';
 import useSectionProps from './utils/useSectionProps';
 import styles from './styles.module.scss';
 
