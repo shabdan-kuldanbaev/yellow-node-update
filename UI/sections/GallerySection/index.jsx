d8f43cc75dba3facf0070bf68eb8463df8741f00
Shabdan <shabdan@mancho.dev>
2022-12-15 21:30:20 +0600

    Fixed mvp gallery section's title

--- UI/sections/GallerySection/index.jsx
+++ UI/sections/GallerySection/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { SwiperNavigation } from 'UI/components/SwiperNavigation';
+import { SwiperSlide } from 'swiper/react';
 import CustomSwiper from 'UI/containers/CustomSwiper';
 import ItemPreview from 'UI/components/ItemPreview';
 import CallToAction from 'UI/components/CallToAction';
