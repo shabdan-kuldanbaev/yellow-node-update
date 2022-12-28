e0aba0322fb522fe2983503700e62369139127ef
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2022-12-28 23:46:03 +0600

    Refactored reffered with Typography components

--- UI/components/Cards/CheckWithText/index.jsx
+++ UI/components/Cards/CheckWithText/index.jsx
@@ -2,8 +2,7 @@ import PropTypes from 'prop-types';
 import Svg from 'UI/components/Svg';
 import CardContainer from 'UI/containers/CardContainer';
 import Typography from 'UI/components/Typography';
-import { TYPOGRAPHY_TAGS } from 'UI/components/Typography/utils/useTypography';
-import { TYPOGRAPHY_SIZE } from 'utils/constants';
+import { TYPOGRAPHY_TAGS, TYPOGRAPHY_SIZE } from 'UI/components/Typography/utils/useTypography';
 import useCheckWithText from './utils/useCheckWithText';
 import styles from './CheckWithText.module.scss';
 
@@ -19,9 +18,13 @@ const CheckWithText = (props) => {
         type="checkFilled"
         className={styles.checkMark}
       />
-      <span className={styles.title}>
+      <Typography
+        size={TYPOGRAPHY_SIZE.headline24}
+        mobileSize={TYPOGRAPHY_SIZE.paragrapgh16}
+        className={styles.title}
+      >
         {children}
-      </span>
+      </Typography>
     </CardContainer>
   );
 };
