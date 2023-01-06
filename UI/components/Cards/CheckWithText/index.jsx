859f8a612bf2c055174336be0e20ade5d73f60d2
Shabdan <shabdan@mancho.dev>
2023-01-06 14:18:53 +0600

    Made changes according comments

--- UI/components/Cards/CheckWithText/index.jsx
+++ UI/components/Cards/CheckWithText/index.jsx
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import Svg from 'UI/components/Svg';
 import CardContainer from 'UI/containers/CardContainer';
-import Typography from 'UI/components/Typography';
-import { TYPOGRAPHY_SIZE } from 'UI/components/Typography/utils/useTypography';
 import useCheckWithText from './utils/useCheckWithText';
 import styles from './CheckWithText.module.scss';
 
@@ -18,13 +16,9 @@ const CheckWithText = (props) => {
         type="checkFilled"
         className={styles.checkMark}
       />
-      <Typography
-        size={TYPOGRAPHY_SIZE.headline24}
-        mobileSize={TYPOGRAPHY_SIZE.paragrapgh16}
-        className={styles.title}
-      >
+      <p className={styles.title}>
         {children}
-      </Typography>
+      </p>
     </CardContainer>
   );
 };
