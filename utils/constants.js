b1c40414c6b98f28f45bb306a1dc678161fd66a6
Shabdan <shabdan@mancho.dev>
2023-01-12 21:15:48 +0600

    Imported refactored Feedback section in switch case container

--- utils/constants.js
+++ utils/constants.js
@@ -196,6 +196,7 @@ export const APP_DEVELOPMENT_TYPES = {
   processOverlay: 'app-development-process-overlay',
   appDevelopmentSliderCards: 'app-development-slider-cards-section',
   appDevelopmentDownloadSection: 'app-development-download-section',
+  feedback: 'feedback',
 };
 
 export const HOMEPAGE_SLOGAN = 'WE CREATE\nFANTASTIC SOFTWARE';
