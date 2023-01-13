49962964b25e44764ccd46c5ed2c94bb07a02c97
Nikita Kuchko <nikita.kuchko@yellow.systems>
2023-01-13 13:26:30 +0300

    Refactor CaseStudiesFooter

---
+++ UI/sections/CaseStudiesFooter/index.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { selectProject } from 'redux/selectors/portfolio';
+import Svg from 'UI/components/Svg';
+import LinkWrapper from 'UI/components/LinkWrapper';
+import { SVG_IMAGES_TYPES } from 'utils/constants';
+import { socialNetworks } from './utils/data';
+import { useCaseStudiesFooter } from './utils/useCaseStudiesFooter';
+import styles from './styles.module.scss';
+
+const CaseStudiesFooter = (props) => {
+  const {
+    type,
+    title,
+    buttonTitle,
+    slug,
+    pathname,
+    footerStyle,
+  } = useCaseStudiesFooter(props);
+
+  return (
+    <footer
+      className={styles[type]}
+      style={footerStyle}
+    >
+      {slug && (
+        <div className={styles.nextProjectContainer}>
+          <p className={styles.subtitle}>
+            Next project
+          </p>
+          <p className={styles.projectName}>
+            {title}
+          </p>
+          <LinkWrapper
+            path={slug}
+            className={styles.more}
+            isLocalLink
+          >
+            <span className={styles.textIndent}>
+              {buttonTitle}
+            </span>
+            <Svg type={SVG_IMAGES_TYPES.arrow} />
+          </LinkWrapper>
+        </div>
+      )}
+      <div className={styles.footerLinks}>
+        <div className={styles.emailContainer}>
+          <p className={styles.question}>
+            Not sure where to start?
+          </p>
+          <LinkWrapper
+            path="mailto:hi@yellow.systems"
+            className={styles.link}
+            isLocalLink
+            googleAnalyticProps={{
+              action: 'Click',
+              data: 'Email',
+            }}
+          >
+            hi@yellow.systems
+          </LinkWrapper>
+        </div>
+        <div>
+          {socialNetworks.map(({
+            name,
+            href,
+            iconType,
+          }) => (
+            <LinkWrapper
+              key={`networks/${name}`}
+              path={href}
+              className={styles.iconLink}
+              googleAnalyticProps={{
+                category: 'Social',
+                label: pathname,
+                data: name,
+              }}
+              isSocialLink
+            >
+              <Svg type={iconType} />
+            </LinkWrapper>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+CaseStudiesFooter.propTypes = {
+  type: PropTypes.string.isRequired,
+  pathname: PropTypes.string.isRequired,
+  currentProject: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default connect(
+  (state) => ({ currentProject: selectProject(state) }),
+)(CaseStudiesFooter);
--- UI/sections/Footer/index.jsx
+++ UI/sections/Footer/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
-import { CASE_STUDIES_SLUGS } from 'utils/constants';
+import { CASE_STUDIES_SLUGS, REFACTORED_CASE_STUDIES_PAGES } from 'utils/constants';
 
 const CaseStudiesFooter = dynamic(() => import('components/CaseStudiesCommon/CaseStudiesFooter'));
+const CaseStudiesFooterRefactored = dynamic(() => import('UI/sections/CaseStudiesFooter'));
 const DefaultFooter = dynamic(() => import('./DefaultFooter'));
 
 const Footer = () => {
   const { query: { project }, pathname } = useRouter();
 
   if (CASE_STUDIES_SLUGS.includes(project)) {
+    if (REFACTORED_CASE_STUDIES_PAGES.includes(project)) {
+      return (
+        <CaseStudiesFooterRefactored
+          pathname={pathname}
+          type={project}
+        />
+      );
+    }
+
     return (
       <CaseStudiesFooter
         pathname={pathname}
