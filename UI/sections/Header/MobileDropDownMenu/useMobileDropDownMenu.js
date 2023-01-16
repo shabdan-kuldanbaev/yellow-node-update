d2ea40252c810066651e5ef9ddc336d17f6f405c
Artyom Kuroptev <artyom.kuroptev@yellow.systems>
2023-01-16 12:08:17 +0000

    Merge pull request #708 in YNW/yellow-site-v2 from YWS-561 to refactor
    Squashed commit of the following:
    
    commit 790b62da1cd4617b3d27fc9ce4f4e12cf6ebbf20
    Author: AZelenkevich <azelenkevich@outlookamusements.com>
    Date:   Fri Jan 13 18:01:54 2023 +0400
    
        fixed code review comments
    
    commit 2eff208489a39c685a2ed2824a655b25ebf26c19
    Author: AZelenkevich <azelenkevich@outlookamusements.com>
    Date:   Fri Jan 13 14:33:46 2023 +0400
    
        Fixed menu sub items color for mobile light theme
    
    commit 2d9ae00a5911ada48d664225ee51e2aa15b1c033
    Author: AZelenkevich <azelenkevich@outlookamusements.com>
    Date:   Fri Jan 13 13:33:06 2023 +0400
    
        Hidden non-existent menu links
    
    commit 7f75509fb4d851e15caf01a056f379564a61fefa
    Author: AZelenkevich <azelenkevich@outlookamusements.com>
    Date:   Thu Jan 12 15:11:40 2023 +0400
    
        Refactoring of header dropdown menu

--- UI/sections/Header/MobileDropDownMenu/useMobileDropDownMenu.js
+++ UI/sections/Header/MobileDropDownMenu/useMobileDropDownMenu.js
@@ -1,38 +1,37 @@
 import { useState } from 'react';
-import {
-  SUB_NAVIGATION_KEYS,
-  SUB_NAVIGATION_ITEMS,
-  SUB_NAVIGATION_LINKS,
-} from 'utils/constants';
+import { SUB_NAVIGATION_ITEMS, SUB_NAVIGATION_LINKS } from 'utils/constants';
 
-export const useDropDownMenu = ({
+export const useMobileDropDownMenu = ({
   isLightTheme,
-  isDropMenuOpened,
   isPageScrolledDown,
   slug,
   closeMobileMenu,
-  closeDropDownMenu,
 }) => {
-  const [activeSubMenu, setActiveSubMenu] = useState(SUB_NAVIGATION_KEYS.services);
+  const [activeSubMenu, setActiveSubMenu] = useState(null);
 
   const subNavigationItems = SUB_NAVIGATION_ITEMS[slug];
   const subNavigationLinks = SUB_NAVIGATION_LINKS[slug];
 
   const handleOnSubMenuClick = (subMenuKey) => (e) => {
     e.stopPropagation();
+
+    if (subMenuKey === activeSubMenu) {
+      setActiveSubMenu(null);
+
+      return;
+    }
+
     setActiveSubMenu(subMenuKey);
   };
 
   const handleOnLinkClick = (subMenuLinkSlug) => () => {
     if (subMenuLinkSlug) {
       closeMobileMenu();
-      closeDropDownMenu();
     }
   };
 
   return {
     isLightTheme,
-    isDropMenuOpened,
     isPageScrolledDown,
     handleOnSubMenuClick,
     handleOnLinkClick,
