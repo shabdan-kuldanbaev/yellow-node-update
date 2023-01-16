bb9fba13f99e7361b8d32bdd9cf2d29bcb236249
Shabdan <shabdan@mancho.dev>
2023-01-16 08:45:10 +0600

    Created blog card component

---
+++ UI/components/Cards/BlogCard/index.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import LinkWrapper from 'UI/components/LinkWrapper';
+import { CATEGORY_TAGS } from 'utils/constants';
+import { formatDate } from 'utils/helper';
+import CustomImage from 'components/Common/CustomImage';
+import useCardProps from './utils/useCardProps';
+import styles from './styles.module.scss';
+
+const BlogCard = (props) => {
+  const {
+    articlePath,
+    categoryPath,
+    imageUrl,
+    imageSizes,
+    publishedAt,
+    title,
+    introduction,
+    categoryTag,
+  } = useCardProps(props);
+
+  return (
+    <LinkWrapper
+      isLocalLink
+      path={articlePath}
+    >
+      <article className={styles.article}>
+        <CustomImage
+          {...imageSizes}
+          src={imageUrl}
+          alt={articlePath}
+          layout="responsive"
+          scale={2}
+          containerClasses={styles.imgContainer}
+        />
+        <div className={styles.articleContent}>
+          <span className={styles.date}>
+            {formatDate(publishedAt)}
+          </span>
+          <h3 className={styles.title}>
+            <span>
+              {title}
+            </span>
+          </h3>
+          <p className={styles.introduction}>
+            {introduction}
+          </p>
+          <LinkWrapper
+            isLocalLink
+            path={categoryPath}
+            className={styles.categoryLink}
+          >
+            <span className={styles.category}>
+              {`#${CATEGORY_TAGS[categoryTag].replace(/\s/g, '')}`}
+            </span>
+          </LinkWrapper>
+        </div>
+      </article>
+    </LinkWrapper>
+  );
+};
+
+export default BlogCard;
