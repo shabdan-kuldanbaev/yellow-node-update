9c5b65bfb3864a55c6b83b54c3cdbf3af70e315e
Shabdan <shabdan@mancho.dev>
2023-01-29 16:37:22 +0600

    refactor: changed logo's color

--- UI/components/Logo/utils/helpers.js
+++ UI/components/Logo/utils/helpers.js
@@ -1,6 +1,8 @@
 import { SVG_IMAGES_TYPES } from 'utils/constants';
 
-export const WHITE_LOGO_TYPE = [];
+export const WHITE_LOGO_TYPE = [
+  'seven-pm-thursday',
+];
 
 export const BLACK_LOGO_TYPE = [];
 
@@ -25,6 +27,7 @@ export const WHITE_TEXT_LOGO = [
   'marketplace-for-clairvoyant-services',
   'fintech-app-for-credit-score',
   'machine-learning-in-real-estate',
+  'seven-pm-thursday',
 ];
 
 export const getLogoType = (type) => {
