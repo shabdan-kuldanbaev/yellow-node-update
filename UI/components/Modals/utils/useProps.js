77fc5d112cd3d47e5ee01e16f391d48ef9dba83e
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2023-01-31 21:06:28 +0600

    Extracted Modal to UI

---
+++ UI/components/Modals/utils/useProps.js
@@ -0,0 +1,42 @@
+import { useEffect, useRef } from 'react';
+import { setOverflowForBody } from 'utils/helper';
+
+const useProps = ({
+  show,
+  close,
+  ...props
+}) => {
+  const modalRef = useRef(null);
+
+  const handleOnClick = ({ target }) => {
+    if (!modalRef?.current.isEqualNode(target)) {
+      return;
+    }
+
+    close();
+  };
+
+  useEffect(() => {
+    setOverflowForBody(show);
+  }, [show]);
+
+  useEffect(() => {
+    const handleOnKeyDown = ({ key }) => {
+      if (key === 'Escape') {
+        close();
+      }
+    };
+
+    document.addEventListener('keydown', handleOnKeyDown);
+
+    return () => document.removeEventListener('keydown', handleOnKeyDown);
+  }, [close]);
+
+  return {
+    props,
+    handleOnClick,
+    modalRef,
+  };
+};
+
+export default useProps;
