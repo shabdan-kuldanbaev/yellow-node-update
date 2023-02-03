a7202d65fe4019eccc24cafb198073fef469dff9
Shabdan <shabdan@mancho.dev>
2023-02-04 05:28:37 +0600

    refactor: refactored intro section in Fernwayer case added logo styles

--- UI/sections/CaseStudiesIntro/index.jsx
+++ UI/sections/CaseStudiesIntro/index.jsx
@@ -7,6 +7,7 @@ const CaseStudiesIntro = (props) => {
   case CASE_STUDIES.stickerbox:
   case CASE_STUDIES.fireaway:
   case CASE_STUDIES.fairy:
+  case CASE_STUDIES.fernwayer:
     return <DefaultIntro {...props} />;
   default:
     return null;
