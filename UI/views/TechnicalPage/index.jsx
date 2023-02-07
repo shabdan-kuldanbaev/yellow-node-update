ea21aed76209ae4e17aaee887e2ad0df336797e5
Artem Kuroptev <artyom.kuroptev@yellow.systems>
2023-02-07 21:37:45 +0600

    Refactored Technical page container

--- UI/views/TechnicalPage/index.jsx
+++ UI/views/TechnicalPage/index.jsx
@@ -1,35 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-import { selectComponents, selectMetaData } from 'redux/selectors/layout';
 import MetaTags from 'components/Common/MetaTags';
 import PageHeader from 'components/Common/PageHeader';
-import FullLayout from 'components/Layout/FullLayout';
 import ContentfulParser from 'components/BlogCommon/Article/ContentfulParser';
-import { rootUrl } from 'utils/helper';
-import { pagesBreadcrumbs } from 'utils/breadcrumbs';
+import FullLayout from 'components/Layout/FullLayout';
+import useProps from './utils/useProps';
 import styles from './styles.module.scss';
 
-const TechnicalPageContainer = ({
-  introSection,
-  type,
-  title,
-}) => {
-  const pageData = useSelector(selectComponents);
-  const metaData = useSelector(selectMetaData);
-
-  const { main: contentModules } = pageData;
-
-  const breadcrumbs = pagesBreadcrumbs.technicalPage(type);
-
-  const pageMetadata = {
-    ...metaData,
-    url: `${rootUrl}/${type}`,
-  };
-
-  if (!pageData || !contentModules) {
-    return null;
-  }
+const TechnicalPageContainer = (props) => {
+  const {
+    type,
+    pageMetadata,
+    breadcrumbs,
+    title,
+    updatedAt,
+    text,
+  } = useProps(props);
 
   return (
     <>
@@ -38,15 +24,16 @@ const TechnicalPageContainer = ({
         pageMetadata={pageMetadata}
         breadcrumbs={breadcrumbs}
       />
-      <FullLayout introSection={introSection}>
+      <FullLayout className={styles.container}>
         <PageHeader
           title={title}
           breadcrumbs={breadcrumbs}
-          updatedAt={contentModules[0].sys.updatedAt}
+          updatedAt={updatedAt}
           breadcrumbsTheme="dark"
+          className={styles.titleStyle}
         />
         <div className={styles.container}>
-          <ContentfulParser document={contentModules[0].fields.text} />
+          <ContentfulParser document={text} />
         </div>
       </FullLayout>
     </>
