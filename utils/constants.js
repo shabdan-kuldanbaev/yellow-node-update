b1948a7fd5a87cfa978c0a5e3180e84a09b6d690
Nikita Kuchko <nikita.kuchko@yellow.systems>
2023-02-15 15:53:23 +0300

    fixed the problem of page order in the header

--- utils/constants.js
+++ utils/constants.js
@@ -454,11 +454,6 @@ export const SUB_NAVIGATION_LINKS = {
         slug: ROUTES.cloudDevelopment.path,
       },
       {
-        title: 'Cloud-based app development services',
-        subtitle: 'No physical space is occupied',
-        slug: ROUTES.lendingSoftwareDevelopment.path,
-      },
-      {
         title: 'Prototyping services',
         subtitle: 'Start with a draft',
         slug: ROUTES.prototypingServices.path,
@@ -483,11 +478,6 @@ export const SUB_NAVIGATION_LINKS = {
         subtitle: 'Lauch your app to all platforms at once',
         slug: ROUTES.crossPlatformDevelopmentServices.path,
       },
-      {
-        title: 'Lending Software Development Company',
-        subtitle: 'Fast and secure lending process',
-        slug: ROUTES.lendingSoftwareDevelopment.path,
-      },
     ],
     [SUB_NAVIGATION_KEYS.expertise]: [
       {
@@ -495,12 +485,6 @@ export const SUB_NAVIGATION_LINKS = {
         subtitle: 'Instant communication tules',
         slug: ROUTES.customChatApp.path,
       },
-      {
-        title: 'Machine learning development services',
-        subtitle: 'Artificial intelligence at your service',
-        slug: ROUTES.mlDevelopment.path,
-      },
-
       {
         title: 'AI software development services',
         subtitle: 'Letting machines think',
@@ -516,6 +500,11 @@ export const SUB_NAVIGATION_LINKS = {
         subtitle: 'Getting insights to boost your business',
         slug: ROUTES.dataScienceDevelopment.path,
       },
+      {
+        title: 'Machine learning development services',
+        subtitle: 'Artificial intelligence at your service',
+        slug: ROUTES.mlDevelopment.path,
+      },
     ],
     [SUB_NAVIGATION_KEYS.fintech]: [
       {
@@ -524,7 +513,7 @@ export const SUB_NAVIGATION_LINKS = {
         slug: ROUTES.fintechDevelopment.path,
       },
       {
-        title: 'Trading platform development company',
+        title: 'Custom Trading Platform Development',
         subtitle: 'The way to manage investments',
         slug: ROUTES.tradingSoftwareDevelopment.path,
       },
