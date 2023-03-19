92b332aefcac74d8ea63747c15e02e4001be84f7
Shabdan <shabdan@mancho.dev>
2023-03-19 23:46:47 +0600

    refactor: refactored case Wireframes section

--- UI/components/Wireframes/utils/herlpers.js
+++ UI/components/Wireframes/utils/herlpers.js
@@ -3,7 +3,6 @@ export const DEFAULT_IMAGE_SIZE = 500;
 
 export const IMAGE_SIZES = {
   'marketplace-for-clairvoyant-services': 541,
-  dindon: 665,
   fernwayer: {
     default: 478,
     secondSectionView: 650,
@@ -76,5 +75,8 @@ export const MOBILE_IMAGE_SIZES = {
   },
   telemojo: {
     default: 206,
+  },
+  dindon: {
+    default: 181,
   },
 };
