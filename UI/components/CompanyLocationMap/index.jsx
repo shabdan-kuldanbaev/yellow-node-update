ebce708e1ce2299495a58b09741aa35f83505593
Artem <artyom.kuroptev@yellow.systems>
2023-04-25 19:05:00 +0600

    Changed default mobile map zoom

--- UI/components/CompanyLocationMap/index.jsx
+++ UI/components/CompanyLocationMap/index.jsx
@@ -1,16 +1,25 @@
-import { useCallback, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import MapGL, { Marker, NavigationControl } from 'react-map-gl';
 import Svg from 'UI/components/Svg';
+import { selectIsMobile } from 'redux/selectors/layout';
+import { useSelector } from 'react-redux';
 import {
   markers,
   INITIAL_MAP_LAT,
   INITIAL_MAP_LNG,
   INITIAL_MAP_ZOOM,
   MAPBOX_TOKEN,
+  INITIAL_MAP_MOBILE_ZOOM,
 } from './utils/data';
 import styles from './styles.module.scss';
 
 const Map = () => {
+  const isMobile = useSelector(selectIsMobile);
+
   const [viewport, setViewport] = useState({
     latitude: INITIAL_MAP_LAT,
     longitude: INITIAL_MAP_LNG,
@@ -21,6 +30,14 @@ const Map = () => {
     (newViewport) => setViewport(newViewport),
     [],
   );
+
+  useEffect(() => {
+    if (!isMobile) {
+      return;
+    }
+
+    setViewport((prev) => ({ ...prev, zoom: INITIAL_MAP_MOBILE_ZOOM }));
+  }, [isMobile]);
 
   return (
     <div className={styles.map}>
--- UI/components/CompanyLocationMap/utils/data.js
+++ UI/components/CompanyLocationMap/utils/data.js
@@ -20,5 +20,6 @@ export const markers = {
 export const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 export const INITIAL_MAP_ZOOM = 1;
+export const INITIAL_MAP_MOBILE_ZOOM = 0.6;
 export const INITIAL_MAP_LNG = -40.539046499999266;
 export const INITIAL_MAP_LAT = 17.515557952842894;
