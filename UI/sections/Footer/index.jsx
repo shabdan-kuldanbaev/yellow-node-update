660a3d73fd63ab25a79e92aa94d06aec2d4220a9
Artem <artyom.kuroptev@yellow.systems>
2023-04-25 20:52:21 +0600

    Renamed refactored component

--- UI/sections/Footer/index.jsx
+++ UI/sections/Footer/index.jsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 import { CASE_STUDIES_SLUGS } from 'utils/constants';
 
-const CaseStudiesFooterRefactored = dynamic(() => import('UI/sections/CaseStudiesFooter'));
+const CaseStudiesFooter = dynamic(() => import('UI/sections/CaseStudiesFooter'));
 const DefaultFooter = dynamic(() => import('./DefaultFooter'));
 
 const Footer = () => {
@@ -10,7 +10,7 @@ const Footer = () => {
 
   if (CASE_STUDIES_SLUGS.includes(project)) {
     return (
-      <CaseStudiesFooterRefactored
+      <CaseStudiesFooter
         pathname={pathname}
         type={project}
       />
