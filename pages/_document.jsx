6d8d656514ff2f86b766b521008327b7cb260e8f
Shabdan Kuldanbaev <noxiousbrainiac@gmail.com>
2023-06-07 15:45:03 +0600

    refactor: - added font "Roboto"

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -49,6 +49,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
           <Script
             id="gtm-script"
             strategy="lazyOnload"
