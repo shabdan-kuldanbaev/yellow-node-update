b728837556486be406a74a9c287ca12f6274f9f0
Shabdan Kuldanbaev <noxiousbrainiac@gmail.com>
2023-06-13 21:33:41 +0600

    refactor: - refactored case results section - added videos - refactored case challenges and solutions case

--- UI/sections/ResultsSection/utils/resultsHelper.js
+++ UI/sections/ResultsSection/utils/resultsHelper.js
@@ -5,5 +5,6 @@ export const isContentVideo = (type) => [
   CASE_STUDIES.balzano,
   CASE_STUDIES.mobileBudgetingApp,
   CASE_STUDIES.forexTradingPlatform,
+  CASE_STUDIES.metapix,
   'why-us',
 ].includes(type);
