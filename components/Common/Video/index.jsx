c752b61b59651ac91a1afcad9ef8a0f57884abc3
Shabdan Kuldanbaev <noxiousbrainiac@gmail.com>
2023-06-14 22:00:36 +0600

    Revert "refactored video component"
    This reverts commit 7477242ee84d717e46be515596e812014a565e3f.

--- components/Common/Video/index.jsx
+++ components/Common/Video/index.jsx
@@ -1,29 +1,19 @@
 import PropTypes from 'prop-types';
-import { useEffect, useRef } from 'react';
 
-const Video = ({ src, className }) => {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    ref?.current?.play();
-  });
-
-  return (
-    <video
-      ref={ref}
-      autoPlay
-      loop
-      muted
-      className={className}
-      playsInline
-    >
-      <source
-        src={src}
-        type="video/mp4"
-      />
-    </video>
-  );
-};
+const Video = ({ src, className }) => (
+  <video
+    autoPlay
+    loop
+    muted
+    className={className}
+    playsInline
+  >
+    <source
+      src={src}
+      type="video/mp4"
+    />
+  </video>
+);
 
 Video.propTypes = {
   src: PropTypes.string.isRequired,
