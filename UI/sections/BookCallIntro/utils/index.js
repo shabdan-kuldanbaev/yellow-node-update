8c54a4c8668c3bf5cfd9f4ad4cd082b580b90e4c
Artem <artyom.kuroptev@yellow.systems>
2023-06-23 18:28:36 +0600

    Moved event fireing to eventSchedueld event

--- UI/sections/BookCallIntro/utils/index.js
+++ UI/sections/BookCallIntro/utils/index.js
@@ -1,7 +1,5 @@
-import { useRouter } from 'next/router';
 import { useCalendlyEventListener } from 'react-calendly';
 import { getDocumentFields } from 'utils/helper';
-import gaHelper from 'utils/ga';
 
 export default function useProps({
   section,
@@ -20,28 +18,11 @@ export default function useProps({
     ],
   );
 
-  const { query: { utm_source: utmSource = null } } = useRouter();
-
   const { url: calendlyEventUrl } = getDocumentFields(contentModules[1], ['url']);
-
-  const handleOnCTAClick = () => {
-    window.gtag(
-      {
-        event: 'book_call_from_videoask',
-      },
-    );
-
-    window.gtag('event', 'book_call_from_videoask');
-    console.log(window.dataLayer);
-  };
 
   useCalendlyEventListener({
     onEventScheduled: (e) => {
-      gaHelper.trackEvent(
-        'Call',
-        'Call Scheduled',
-        { campaignSource: utmSource },
-      );
+      window.gtag('event', 'book_call_from_videoask');
     },
   });
 
@@ -50,8 +31,6 @@ export default function useProps({
     subtitle,
     contentModules,
     calendlyEventUrl,
-    utmSource,
     ...restProps,
-    handleOnCTAClick,
   };
 }
