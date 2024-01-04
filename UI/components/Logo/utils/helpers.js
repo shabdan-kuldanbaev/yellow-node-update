bba993b9720cff666bd4d2be83a0bff9ba6bf210
shabdankuldanbaev-yellow <shabdan.kuldanbaev@yellow.systems>
2024-01-04 19:12:15 +0600

    Refactored Header logo

--- UI/components/Logo/utils/helpers.js
+++ UI/components/Logo/utils/helpers.js
@@ -1,4 +1,4 @@
-import { CASE_STUDIES, SVG_IMAGES_TYPES } from 'utils/constants';
+import { CASE_STUDIES, ROUTES, SVG_IMAGES_TYPES } from 'utils/constants';
 
 export const WHITE_LOGO_TYPE = [
   CASE_STUDIES.fireaway,
@@ -50,8 +50,7 @@ export const BLACK_LOGO_TYPE = [
 ];
 
 export const WHITE_TEXT_LOGO = [
-  'home',
-  'footer',
+  ROUTES.homepage.path,
   CASE_STUDIES.stickerbox,
   CASE_STUDIES.natp,
   CASE_STUDIES.travelTrivia,
