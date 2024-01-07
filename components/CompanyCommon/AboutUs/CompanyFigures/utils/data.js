2df46d02809a1b1e94c855d36072f96945abd8b2
shabdankuldanbaev-yellow <shabdan.kuldanbaev@yellow.systems>
2024-01-07 15:03:59 +0600

    Removed unused old common components

--- components/CompanyCommon/AboutUs/CompanyFigures/utils/data.js
+++
@@ -1,6 +0,0 @@
-export const companyFiguresData = [
-  { number: 70, isPlus: true, title: 'developers and designers' },
-  { number: 6, isPlus: false, title: 'years on the market' },
-  { number: 20, isPlus: true, title: 'million users' },
-  { number: 1, isPlus: false, title: 'PayPal vulnerability detected' },
-];
